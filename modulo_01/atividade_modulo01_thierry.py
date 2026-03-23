d4447ddd985ed27c850ce9885b90a213e3d5f850
thierry <ferreirathierry647@gmail.com>
2026-03-23 13:31:22 -0300

    Create atividade_modulo01_thierry.py
    atividade do modulo 1 da plataforma

---
+++ modulo_01/atividade_modulo01_thierry.py
@@ -0,0 +1,12 @@
+print('Digite a idade do jovem: ')
+
+idade = int(input()) 
+
+if idade <=18:
+    print('jovem é menor de idade')
+
+elif idade <=65:
+      print('jovem é maior de idade')
+
+else:
+     print('Jovem é idoso de idade')

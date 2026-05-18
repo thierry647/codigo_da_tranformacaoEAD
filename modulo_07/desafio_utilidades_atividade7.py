a94acff7f039ff8dfa1b026148792275d0cf8578
programacao2026 <ferreirathierry647@gmail.com>
2026-05-18 14:01:55 -0300

    update desafio 7
    com base no repositorio do professor e nas aulas em video se quarta

---
+++ modulo_07/desafio_utilidades_atividade7.py
@@ -0,0 +1,24 @@
+'''
+
+utilidades
+
+'''
+# Arquivo: utilidades.py
+
+def somar(a, b):
+    """
+    Soma dois números.
+    """
+    return a + b
+
+def subtrair(a, b):
+    """
+    Subtrai o segundo número do primeiro.
+    """
+    return a - b
+
+def potencia(base, expoente):
+    """
+    Calcula a potência de um número.
+    """
+    return base ** expoente

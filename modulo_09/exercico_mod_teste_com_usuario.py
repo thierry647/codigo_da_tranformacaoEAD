51dde586430b859d010f88b88563be580d6b48e5
programacao2026 <ferreirathierry647@gmail.com>
2026-05-18 14:07:47 -0300

    update

---
+++ modulo_09/exercico_mod_teste_com_usuario.py
@@ -0,0 +1,30 @@
+'''
+
+
+'''
+class Celular:
+    def __init__(self, marca, modelo):
+        
+        self.marca,self.modelo = marca, modelo
+        
+        self.bateria = 100
+        
+     
+     
+def fazer_chamada(self, custo_bateria):
+    print(f"\n ---Chamada no {self.modelo} ---")
+    
+    try:
+        self.bateria -= custo_bateria
+
+    except TypeError:
+        print("Erro: O custo da bateria deve ser um numero")
+
+    else:
+        print(f"Sucesso! Bateria atual: {self.bateria}%")
+
+    finally:
+        print("Sistema finalizado.")
+
+
+                   
